fdae45c3336f7e8591f206341541d8962742ddba
Bertrand6294 <52163805+Bertrand6294@users.noreply.github.com>
2019-06-24 08:54:24 -0400

    Add files via upload
    added p2t1 file

---
+++ P2T1_SalesPrediction_TrevorBertrand.py
@@ -0,0 +1,13 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Practicing Python Programming
+>>> #6/18/19
+>>> #CTI-110 P2T1-Sales Prediction
+>>> #Trevor Bertrand
+>>> #
+>>> total_sales = float(input('Enter the projected sales: '))
+Enter the projected sales: 100000000
+>>> profit = total_sales * 0.23
+>>> print ('The profit is $', profit)
+The profit is $ 23000000.0
+>>> 
